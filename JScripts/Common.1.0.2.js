bc7396bd44dd67b481e802de37d914bd3904147d
KeatSHOS <168560516+KeatSHOS@users.noreply.github.com>
2025-08-04 10:39:59 -0400

    Update Common.1.0.2.js

--- JScripts/Common.1.0.2.js
+++ JScripts/Common.1.0.2.js
@@ -1,1 +1,8 @@
-alert(1);
+
+(function waitForTelerik() {
+  if (typeof Telerik !== 'undefined') {
+    alert(1);
+  } else {
+    setTimeout(waitForTelerik, 100); // Poll every 100 milliseconds
+  }
+})();
