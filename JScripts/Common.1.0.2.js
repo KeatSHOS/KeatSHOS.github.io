8e1f7dbf9d147d77d1865f5fb8f6a21d490aa248
KeatSHOS <168560516+KeatSHOS@users.noreply.github.com>
2025-08-04 10:54:08 -0400

    Update Common.1.0.2.js

--- JScripts/Common.1.0.2.js
+++ JScripts/Common.1.0.2.js
@@ -1,22 +1,4 @@
-
-(function waitForTelerik() {
-  if (
-    typeof Telerik !== 'undefined' &&
-    Telerik.Web &&
-    Telerik.Web.UI &&
-    Telerik.Web.UI.RadTreeView
-  ) {
-    Telerik.Web.UI.RadTreeView.prototype.saveClientState = function () {
-      return "{\"expandedNodes\":" + this._expandedNodesJson +
-        ",\"collapsedNodes\":" + this._collapsedNodesJson +
-        ",\"logEntries\":" + this._logEntriesJson +
-        ",\"selectedNodes\":" + this._selectedNodesJson +
-        ",\"checkedNodes\":" + this._checkedNodesJson +
-        ",\"scrollPosition\":" + Math.round(this._scrollPosition) + "}";
-    };
-    alert(1);
-  } else {
-    setTimeout(waitForTelerik, 100); // Retry every 100ms
-  }
-})();
+setTimeout(function() {
+  alert(1);
+}, 10000);
 
